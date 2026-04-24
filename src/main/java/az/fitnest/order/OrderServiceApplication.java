981f97b0545328b9bb470621111020be4014cb6f
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-04-24 21:16:17 +0400

    chore: disable Redis repository scanning

--- src/main/java/az/fitnest/order/OrderServiceApplication.java
+++ src/main/java/az/fitnest/order/OrderServiceApplication.java
@@ -3,10 +3,12 @@ package az.fitnest.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "az.fitnest.order.repository")
+@EnableRedisRepositories(basePackages = {})
 @EnableScheduling
 public class OrderServiceApplication {
 
