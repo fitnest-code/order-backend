3c1b896cdc0d1bc7cbc333b49d6129634057395b
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-05-21 12:07:01 +0400

    Optimize caching and UI fixes

--- src/main/java/az/fitnest/order/config/RedisConfig.java
+++ src/main/java/az/fitnest/order/config/RedisConfig.java
@@ -17,6 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@org.springframework.cache.annotation.EnableCaching
 public class RedisConfig {
 
     @Bean
