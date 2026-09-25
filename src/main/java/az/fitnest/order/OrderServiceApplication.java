49fa77e56467fc60acf08202a6863ea5d3cb5f3b
nijataghayev <nicat.agayev.536@gmail.com>
2026-09-25 20:05:54 +0400

    fix(timezone): set JVM default timezone and jackson timezone to Asia/Baku (UTC+4) to eliminate 4-hour server time offset

--- src/main/java/az/fitnest/order/OrderServiceApplication.java
+++ src/main/java/az/fitnest/order/OrderServiceApplication.java
@@ -1,10 +1,13 @@
 package az.fitnest.order;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "az.fitnest.order.repository")
@@ -12,6 +15,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @org.springframework.scheduling.annotation.EnableAsync
 public class OrderServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Baku"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
